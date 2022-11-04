52f3501015adc6c662402b82510d73a250c57af5
Gonzalo Zamorano <g.zamorano256@gmail.com>
2022-11-04 20:48:09 -0300

    clase EstadoDesafio hecha

---
+++ src/estadoDesafio/EstadoDesafio.java
@@ -0,0 +1,15 @@
+package estadoDesafio;
+
+import main.DesafioDelUsuario;
+
+public abstract class EstadoDesafio {
+
+	public abstract void aceptarDesafio(DesafioDelUsuario desafio);
+
+	public abstract void superarDesafio(DesafioDelUsuario desafioDelUsuario);
+
+	public abstract void incrementarCantidadMuestrasRecolectadas(DesafioDelUsuario desafioDelUsuario);
+
+	public abstract void calificarDesafio(DesafioDelUsuario desafioDelUsuario, Integer puntuacion);
+	
+}
