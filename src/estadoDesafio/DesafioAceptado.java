4cbf96cec0309630cd3157dc4a77174194af35c0
Gonzalo Zamorano <g.zamorano256@gmail.com>
2022-11-04 20:47:28 -0300

    clase DesafioAceptado hecha

---
+++ src/estadoDesafio/DesafioAceptado.java
@@ -0,0 +1,28 @@
+package estadoDesafio;
+
+import main.DesafioDelUsuario;
+
+public class DesafioAceptado extends EstadoDesafio {
+
+	@Override
+	public void aceptarDesafio(DesafioDelUsuario desafio) {
+		// no hace nada, el desafio ya fue aceptado
+	}
+
+	@Override
+	public void superarDesafio(DesafioDelUsuario desafioDelUsuario) {
+		desafioDelUsuario.setEstado(new DesafioCompletado());
+	}
+
+	@Override
+	public void incrementarCantidadMuestrasRecolectadas(DesafioDelUsuario desafioDelUsuario) {
+		desafioDelUsuario.setCantidadMuestrasRecolectadas(desafioDelUsuario.getCantidadMuestrasRecolectadas()+1);
+		desafioDelUsuario.comprobarCompletitud();
+	}
+
+	@Override
+	public void calificarDesafio(DesafioDelUsuario desafioDelUsuario, Integer puntuacion) {
+		// no hace nada, el desafio todavía está en curso
+	}
+
+}
