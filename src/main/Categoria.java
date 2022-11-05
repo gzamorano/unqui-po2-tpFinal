ae3592cf92834094e720df8b0dfcc278f4818147
Fabrizio Amendola <fabrizzioamendola@gmail.com>
2022-11-05 05:47:06 -0300

    implementa clase Categoria y su test

--- src/main/Categoria.java
+++ src/main/Categoria.java
@@ -1,5 +1,29 @@
 package main;
 
 public class Categoria {
+	
+	private String nombre;
+	private String descripcion;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public Categoria(String nombre, String descripcion) {
+		this.setNombre(nombre);
+		this.setDescripcion(descripcion);
+	}
 
 }
--- test/main/CategoriaTest.java
+++ test/main/CategoriaTest.java
@@ -6,14 +6,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CategoriaTest {
+	
+	Categoria categoria;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		categoria = new Categoria("Biología", "Todo lo relacionado con Biología");
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConstructor() {
+		assertEquals(categoria.getNombre(), "Biología");
+		assertEquals(categoria.getDescripcion(), "Todo lo relacionado con Biología");
 	}
 
 }
