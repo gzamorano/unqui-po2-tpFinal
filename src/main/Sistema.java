c063b2b3414cf703b1cd5e84dc17b490fae9e0f7
Gonzalo Zamorano <g.zamorano256@gmail.com>
2022-11-07 12:54:53 -0300

    correcion inst var desuso

--- src/main/Sistema.java
+++ src/main/Sistema.java
@@ -9,20 +9,15 @@ public class Sistema {
 	
 	private List<Categoria> categorias = new ArrayList<Categoria>();
 	private List<Proyecto> proyectos = new ArrayList<Proyecto>();
-	private Busqueda busquedaDeProyectos;
+	//private Busqueda busquedaDeProyectos;
 	
 	
 	public List<Categoria> getCategorias() {
 		return categorias;
 	}
-	public void setCategorias(List<Categoria> categorias) {
-		this.categorias = categorias;
-	}
+
 	public List<Proyecto> getProyectos() {
 		return proyectos;
-	}
-	public void setProyectos(List<Proyecto> proyectos) {
-		this.proyectos = proyectos;
 	}
 	
 	public void addProyecto(Proyecto proyecto) {
