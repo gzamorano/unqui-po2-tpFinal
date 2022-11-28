eebe045eaa6efe9a2f8e71e12edbb750a522c845
Fabrizio Amendola <fabrizzioamendola@gmail.com>
2022-11-28 04:14:32 -0300

    añade tests para filtro CoincidirTitulo

---
+++ test/buscadorDeProyectos/CoincidirTituloTest.java
@@ -0,0 +1,41 @@
+package buscadorDeProyectos;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.Proyecto;
+
+class CoincidirTituloTest {
+	
+	String titulo;
+	CoincidirTitulo filtroTitulo;
+	Proyecto proyecto;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		titulo = "ética";
+		filtroTitulo = new CoincidirTitulo(titulo);
+		proyecto = mock(Proyecto.class);
+	}
+
+	@Test
+	void testConstructor() {
+		assertEquals(filtroTitulo.getTitulo(), "ética");
+	}
+	
+	@Test
+	void testUnProyectoCoincideConElTitulo() {
+		when(proyecto.getNombre()).thenReturn(titulo);
+		assertTrue(filtroTitulo.cumple(proyecto));
+	}
+	
+	@Test
+	void testUnProyecto_No_CoincideConElTitulo() {
+		when(proyecto.getNombre()).thenReturn("ñam fri frufi fali fru");
+		assertFalse(filtroTitulo.cumple(proyecto));
+	}
+
+}
